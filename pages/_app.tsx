d8c45b1c1e7ddfde310dbada2ed72e776eb924ef
Pablo Verduzco <pabloverduzcos@gmail.com>
2021-04-14 12:40:24 -0500

    chore: early webpack 5 adopting

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 // Next
 import Head from 'next/head';
-import { AppProps } from 'next/app';
+import type { AppProps } from 'next/app';
 
 // Components
 import Navbar from 'components/navbar';
@@ -14,11 +14,8 @@ export default function CustomApp({ Component, pageProps }: AppProps): JSX.Eleme
         <title>Pablo Verduzco</title>
         <link rel='preconnect' href='https://fonts.gstatic.com' />
         <link
-          href='https://fonts.googleapis.com/css2?family=DM+Mono:wght@400;500&family=DM+Sans:wght@400;700&display=swap'
-          rel='print'
-          // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-          // @ts-ignore
-          onLoad="this.media='all'"
+          href='https://fonts.googleapis.com/css2?family=DM+Mono:wght@400;500&family=DM+Sans:wght@400;500&display=swap'
+          rel='stylesheet'
         />
       </Head>
       <Navbar />
